d48a7d85158932a0eb6d978748266809fe6b3e4b
smorty <hauh@ya.ru>
2019-10-17 18:54:37 +0300

    ex02 ex 03

--- d07/ex02/Targaryen.class.php
+++ d07/ex02/Targaryen.class.php
@@ -1,6 +1,15 @@
 <?php
 class Targaryen
 {
+	protected function resistsFire() {
+		return False;
+	}
 
+	public function getBurned() {
+		if ($this->resistsFire())
+			return "emerges naked but unharmed";
+		else
+			return "burns alive";
+	}
 }
 ?>
